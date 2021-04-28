2f4203496233a894783a3a8d6816e43aa60cccad
duvan <duvanli@hotmail.es>
2021-04-28 18:15:04 -0500

    mirnado el web hook

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,13 +6,13 @@ const path= require('path');
 
 
 router.post('/', async function (req ,res) {
-    fs.writeFileSync(path.join(__dirname +'../public/reshook.json'),JSON.stringify(req.body))
     console.log(req.body)
+    fs.writeFileSync(path.join(__dirname,'../public/reshook.json'),JSON.stringify(req.body))
     return res.status(200);
  })
 
 router.get('/download', function(req, res){
-    res.download(path.join(__dirname +'../public/reshook.json'));
+    res.download(path.join(__dirname,'../public/reshook.json'));
 });
 
 module.exports = router;
