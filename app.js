bf655382bb3ad1c30ce1b5909fd31e942639af20
duvan <duvanli@hotmail.es>
2021-04-28 11:52:21 -0500

    rutas en server funcionable

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use( express.json() );
+ 
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+});
 
-app.use( express.json() ); 
 
 app.use( '/api/products',require('./routes/products')); 
 
