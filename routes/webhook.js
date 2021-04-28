e8945e73359fc2bc2e22faabaabdc3422ad3c24e
duvan <duvanli@hotmail.es>
2021-04-28 18:01:29 -0500

    Mirando error de webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,16 +1,18 @@
 const { Router } = require('express');
 const router = Router();
 const fs = require('fs');
+const path= require('path');
+
 
 
 router.post('/', async function (req ,res) {
-    fs.writeFileSync('../json/reshook.json',JSON.stringify(req.body))
+    fs.writeFileSync(path.join(__dirname +'/public/reshook.json'),JSON.stringify(req.body))
     console.log(req.body)
     return res.status(200);
  })
 
 router.get('/download', function(req, res){
-    res.download('ruta del archivo y nombre');
+    res.download(path.join(__dirname +'/public/reshook.json'));
 });
 
 module.exports = router;
